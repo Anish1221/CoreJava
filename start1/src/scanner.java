c2c36c49e601e29eacc4465bfbb12f9acb20f0f5
unknown <anis.anoyin@hotmail.com>
2016-05-25 22:00:23 +0545

    hw

---
+++ start1/src/scanner.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Annoyin
+ */
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args){
+        System.out.println("Enter your name:");
+        Scanner x =new Scanner(System.in);
+        String name=x.nextLine();
+        System.out.println(name);
+                
+    }
+    
+}
